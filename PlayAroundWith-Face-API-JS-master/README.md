feb606761e1b1e633540c462213f0356ba4b14c8
thismeiko <114015040+thismeiko@users.noreply.github.com>
2022-10-26 00:00:48 -0500

    new

---
+++ PlayAroundWith-Face-API-JS-master/README.md
@@ -0,0 +1,6 @@
+# PlayAroundWith-Face-API-JS
+
+Reference:<br/>
+https://github.com/justadudewhohacks/face-api.js<br/>
+https://github.com/WebDevSimplified/Face-Detection-JavaScript<br/>
+https://www.youtube.com/watch?v=CVClHLwv-4I&feature=youtu.be
--- PlayAroundWith-Face-API-JS-master/script.js
+++ PlayAroundWith-Face-API-JS-master/script.js
@@ -21,25 +21,19 @@ video.addEventListener('play', () => {
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
+
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(
       video, 
-      new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withAgeAndGender().withFaceDescriptors()
+      new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withAgeAndGender().withFaceDescriptors().withFaceExpressions()
     
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
-    resizedDetections.forEach(result => {
-      const { age, gender, genderProbability } = result
-      new faceapi.draw.DrawTextField(
-        [
-          `${faceapi.round(age, 0)} years`,
-          `${gender} (${faceapi.round(genderProbability)})`
-        ],
-        result.detection.box.bottomLeft
-      ).draw(canvas)
-    })
+    
+
   }, 100)
 })

5662a40c1c4bfd94333135b0dc789195f740fdca
thismeiko <114015040+thismeiko@users.noreply.github.com>
2022-10-27 00:01:34 -0500

    new

--- PlayAroundWith-Face-API-JS-master/script.js
+++ PlayAroundWith-Face-API-JS-master/script.js
@@ -6,43 +6,34 @@ Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
   faceapi.nets.faceExpressionNet.loadFromUri('./models'),
   faceapi.nets.ageGenderNet.loadFromUri('./models'),
-]).then(startVideo)
+]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
-  )
+  );
 }
 
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+
+  faceapi.matchDimensions(canvas, displaySize);
+
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(
       video, 
       new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withAgeAndGender().withFaceDescriptors().withFaceExpressions()
     
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
 
-let button;
-
-function setup(){
-button = createButton('click me');
-button.position(700,700);
-button.mousePressed(takePhotos);
-}
-
-function takePhotos(){
-
-}
 
